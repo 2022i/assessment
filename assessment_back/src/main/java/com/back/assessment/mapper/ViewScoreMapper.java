7536501711ae4a99622b388b96c032514e31f44d
li.zezhi <2383195232@qq.com>
2024-07-02 17:25:15 +0800

    修改

---
+++ assessment_back/src/main/java/com/back/assessment/mapper/ViewScoreMapper.java
@@ -0,0 +1,20 @@
+package com.back.assessment.mapper;
+
+import com.back.assessment.entity.ViewScore;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+* @author 23831
+* @description 针对表【view_score】的数据库操作Mapper
+* @createDate 2024-07-02 11:09:51
+* @Entity com.back.assessment.entity.ViewScore
+*/
+@Mapper
+public interface ViewScoreMapper extends BaseMapper<ViewScore> {
+
+}
+
+
+
+
